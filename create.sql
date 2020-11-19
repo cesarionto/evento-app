09a183d3562806c67f7fa0b89742f63181d89282
cesarionto <cesariopereiraneto@gmail.com>
2020-11-19 00:38:49 -0300

    Adiciona evento

---
+++ create.sql
@@ -0,0 +1,6 @@
+create sequence hibernate_sequence start 1 increment 1;
+create table evento (id int8 not null, data varchar(255), horario varchar(255), local varchar(255), nome varchar(255), primary key (id));
+create sequence hibernate_sequence start 1 increment 1;
+create table evento (id int8 not null, data varchar(255), horario varchar(255), local varchar(255), nome varchar(255), primary key (id));
+create sequence hibernate_sequence start 1 increment 1;
+create table evento (id int8 not null, data varchar(255), horario varchar(255), local varchar(255), nome varchar(255), primary key (id));
--- src/main/java/com/cesario/eventoapp/controllers/EventoController.java
+++ src/main/java/com/cesario/eventoapp/controllers/EventoController.java
@@ -3,14 +3,15 @@ package com.cesario.eventoapp.controllers;
 import com.cesario.eventoapp.models.Evento;
 import com.cesario.eventoapp.repository.EventoRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@AllArgsConstructor
 public class EventoController {
 
+    @Autowired
     private EventoRepository eventoRepository;
 
     @RequestMapping(value = "cadastrarEvento", method = RequestMethod.GET)
