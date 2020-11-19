7081acd26d47ba7ac73967ed7d68789ca2d6e596
cesarionto <cesariopereiraneto@gmail.com>
2020-11-19 00:01:00 -0300

    Criando tela de cadastro de eventos

---
+++ src/main/java/com/cesario/eventoapp/controllers/EventoController.java
@@ -0,0 +1,12 @@
+package com.cesario.eventoapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EventoController {
+    @RequestMapping("cadastrarEvento")
+    public String formEvento(){
+        return "evento/formEvento";
+    }
+}
